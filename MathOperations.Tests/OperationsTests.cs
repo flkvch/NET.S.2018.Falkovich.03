4e8a111c50e04e88b35d295477fa488d4bef33a9
Ksenia <ksenya-f3@rambler.ru>
2018-06-30 12:56:24 +0300

    Add project files.

---
+++ MathOperations.Tests/OperationsTests.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace MathOperations.Tests
+{
+    [TestFixture]
+    public class OperationsTests
+    {
+        [TestCase(1, 5, 0.0001, ExpectedResult = 1)]
+        public static double FindNthRoot_RootFromOne(double number, int n, double accuracy)
+        => Operations.FindNthRoot(1, 5, 0.0001);
+
+        [TestCase(8, 3, 0.0001, ExpectedResult = 2)]
+        public static double FindNthRoot_IntegerAnswer(double number, int n, double accuracy)
+            => Operations.FindNthRoot(8, 3, 0.0001);
+
+        [TestCase(0.001, 3, 0.0001, ExpectedResult = 0.1)]
+        public static double FindNthRoot_1(double number, int n, double accuracy)
+            => Operations.FindNthRoot(0.001, 3, 0.0001);
+
+        [TestCase(0.04100625, 4, 0.0001, ExpectedResult = 0.45)]
+        public static double FindNthRoot_2(double number, int n, double accuracy)
+            => Operations.FindNthRoot(0.04100625, 4, 0.0001);
+
+        [TestCase(0.0279936, 7, 0.0001, ExpectedResult = 0.6)]
+        public static double FindNthRoot_BigPower(double number, int n, double accuracy)
+            => Operations.FindNthRoot(0.0279936, 7, 0.0001);
+
+        [TestCase(0.0081, 4, 0.1, ExpectedResult = 0.3)]
+        public static double FindNthRoot_3(double number, int n, double accuracy)
+            => Operations.FindNthRoot(0.0081, 4, 0.1);
+
+        [TestCase(-0.008, 3, 0.1, ExpectedResult = -0.2)]
+        public static double FindNthRoot_FromNegative(double number, int n, double accuracy)
+            => Operations.FindNthRoot(-0.008, 3, 0.1);
+
+        [TestCase(0.004241979, 9, 0.00000001, ExpectedResult = 0.545)]
+        public static double FindNthRoot_BigAccuracy(double number, int n, double accuracy)
+            => Operations.FindNthRoot(0.004241979, 9, 0.00000001);
+
+        [Test]
+        public void FindNthRoot_NegativeNumberEvenPow_Exception() =>
+            Assert.Throws<ArgumentException>(() => Operations.FindNthRoot(-0.001, 2, 0.0001));
+
+        [Test]
+        public void FindNthRoot_NegativePow_Exception() =>
+    Assert.Throws<ArgumentException>(() => Operations.FindNthRoot(0.001, -2, 0.0001));
+        [Test]
+        public void FindNthRoot_UncorrectAccuracy_Exception() =>
+    Assert.Throws<ArgumentException>(() => Operations.FindNthRoot(0.001, 2, -1));
+    }
+}
---
+++ MathOperations/Operations.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathOperations
+{
+    public static class Operations
+    {
+        /// <summary>
+        /// Accracy of calculating
+        /// </summary>
+        private const double X0 = 0.1;
+
+        public static double FindNthRoot(double number, int n, double accuracy)
+        {
+            if (!IsValid(number, n, accuracy))
+            {
+                throw new ArgumentException();
+            }
+
+            double root = NewtonCalc(number, n, accuracy);
+            return Round(root, accuracy);
+        }
+
+        private static double Round(double root, double accuracy)
+        {
+            int count = -1;
+            while (accuracy < 10)
+            {
+                int digit = (int)accuracy;
+                count++;
+                accuracy *= 10;
+            }
+
+            return Math.Round(root, count);
+        }
+
+        private static double NewtonCalc(double number, int n, double accuracy)
+        {
+            double xk = X0, xkp1 = X0;
+            for (;;)
+            {
+                xkp1 = 1.0 / n * ((n - 1) * xk + number / Math.Pow(xk, n - 1));
+                if (Math.Abs(xkp1 - xk) > accuracy)
+                {
+                    xk = xkp1;
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            return xkp1;
+        }
+
+        private static bool IsValid(double number, int n, double accuracy)
+        {
+            if (n % 2 == 0)
+            {
+                if (number < 0)
+                {
+                    return false;
+                }
+            }
+
+            if (n < 2)
+            {
+                    return false;
+            }
+
+            if (accuracy < 1 && accuracy > 0)
+            {
+                double temp = accuracy;
+                while (temp < 10)
+                {
+                    int digit = (int)temp;
+                    if (digit > 1)
+                    {
+                        return false;
+                    }
+                    else
+                    {
+                        temp *= 10;
+                    }
+                }
+
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
