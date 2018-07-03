933af1cb429b22136149be672088ac2b567c4347
Ksenia <ksenya-f3@rambler.ru>
2018-07-03 19:33:31 +0300

    Deleted ArrayResize from the cycle

---
+++ MathOperations.Tests/GCDTests.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using NUnit.Framework;
+using MathOperations;
+using System.Diagnostics;
+
+namespace MathOperations.Tests
+{
+    [TestFixture]
+    public class GCDTests
+    {
+        [TestCase(25, 50, ExpectedResult = 25)]
+        [TestCase(50, 25, ExpectedResult = 25)]
+        [TestCase(25, 1, ExpectedResult = 1)]
+        [TestCase(120, 50, ExpectedResult = 10)]
+        [TestCase(-25, 50, ExpectedResult = 25)]
+        [TestCase(17, 7, ExpectedResult = 1)]
+        [TestCase(0, 50, ExpectedResult = 50)]
+        [TestCase(0, 0, ExpectedResult = 0)]
+        [TestCase(55, 55, ExpectedResult = 55)]
+        public static int FindGCD_TwoNumbers(int a, int b) => GCD.FindGCD(a, b);
+
+        [TestCase(25, 50, 100, ExpectedResult = 25)]
+        [TestCase(120, 50, 3, ExpectedResult = 1)]
+        [TestCase(-25, 50, -100, ExpectedResult = 25)]
+        [TestCase(25, 0, 50, ExpectedResult = 25)]
+        [TestCase(47, 7, 3, ExpectedResult = 1)]
+        public static int FindGCD_ThreeNumbers(params int[] array)
+        => GCD.FindGCD(array);
+
+        [TestCase(15)]
+        public static void FindGCD_OneNumber(params int[] array)
+        => Assert.Throws<ArgumentException>(() => GCD.FindGCD(array));
+
+        [TestCase(25, 50, ExpectedResult = 25)]
+        [TestCase(50, 25, ExpectedResult = 25)]
+        [TestCase(25, 1, ExpectedResult = 1)]
+        [TestCase(120, 50, ExpectedResult = 10)]
+        [TestCase(-25, 50, ExpectedResult = 25)]
+        [TestCase(17, 7, ExpectedResult = 1)]
+        [TestCase(0, 50, ExpectedResult = 50)]
+        [TestCase(0, 0, ExpectedResult = 0)]
+        [TestCase(55, 55, ExpectedResult = 55)]
+        public static int FindGCDBinary_TwoNumbers(int a, int b) 
+            => GCD.FindGCDBinary(a, b);
+
+        [TestCase(25, 50, 100, ExpectedResult = 25)]
+        [TestCase(120, 50, 3, ExpectedResult = 1)]
+        [TestCase(-25, 50, -100, ExpectedResult = 25)]
+        [TestCase(25, 0, 50, ExpectedResult = 25)]
+        [TestCase(47, 7, 3, ExpectedResult = 1)]
+        public static int FindGCDBinary_ThreeNumbers(params int[] array)
+        => GCD.FindGCDBinary(array);
+
+        [TestCase("", 25, 50, 100, ExpectedResult = 25)]
+        [TestCase("", 120, 50, 3, ExpectedResult = 1)]
+        [TestCase("", -25, 50, -100, ExpectedResult = 25)]
+        [TestCase("", 25, 0, 50, ExpectedResult = 25)]
+        [TestCase("", 47, 7, 3, ExpectedResult = 1)]
+        public static int FindGCD_ThreeNumbersOutTime(out string time, params int[] array)
+        {
+            int a = GCD.FindGCD(out time, array);
+            Console.WriteLine(time);
+            return a;
+        }
+
+        [TestCase("", 25, 50, 100, ExpectedResult = 25)]
+        [TestCase("", 120, 50, 3, ExpectedResult = 1)]
+        [TestCase("", -25, 50, -100, ExpectedResult = 25)]
+        [TestCase("", 25, 0, 50, ExpectedResult = 25)]
+        [TestCase("", 47, 7, 3, ExpectedResult = 1)]
+        public static int FindGCDBinary_ThreeNumbersOutTime(out string time, params int[] array)
+        {
+            int a = GCD.FindGCDBinary(out time, array);
+            Console.WriteLine(time);
+            return a;
+        }
+    }
+}
---
+++ MathOperations/GCD.cs
@@ -0,0 +1,226 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace MathOperations
+{
+    /// <summary>
+    /// Algorithms of finding the greatest common divisor
+    /// </summary>
+    public static class GCD
+    {
+        /// <summary>
+        /// Finds GCD of 2 numbers <paramref name="a"/> and <paramref name="b"/> by Euclidian's algorithm 
+        /// </summary>
+        /// <param name="a">
+        /// The first number
+        /// </param>
+        /// <param name="b">
+        /// The second number
+        /// </param>
+        /// <returns>
+        /// The greatest common divisor of <paramref name="a"/> and <paramref name="b"/>
+        /// </returns>
+        public static int FindGCD(int a, int b)
+        {
+            if (a == b)
+            {
+                return a;
+            }
+
+            if (a == 0)
+            {
+                return b;
+            }
+
+            if (b == 0)
+            {
+                return a;
+            }
+
+            a = Math.Abs(a);
+            b = Math.Abs(b);
+            while (a != b)
+            {
+                if (a > b)
+                {
+                    a -= b;
+                }
+                else
+                {
+                    b -= a;
+                }
+            }
+
+            return a;
+        }
+
+        /// <summary>
+        /// Finds GCD of 3 and more numbers by Euclidian's algorithm 
+        /// </summary>
+        /// <param name="array">
+        /// Numbers 
+        /// </param>
+        /// <returns>
+        /// GCD of 3 and more numbers
+        /// </returns>
+        public static int FindGCD(params int[] array)
+        {
+            if (array.Length == 1)
+            {
+                throw new ArgumentException(nameof(array), "CGD counts only for 2 and more numbers");
+            }
+
+            int gcd = array[0];
+            for (int i = 1; i < array.Length; i++)
+            {
+                gcd = FindGCD(gcd, array[i]);
+            }
+
+            return gcd;
+        }
+
+        /// <summary>
+        /// Finds GCD of 3 and more numbers by Euclidian's algorithm 
+        /// </summary>
+        /// <param name="time">
+        /// Returns elapsed time for the algorithm 
+        /// </param>
+        /// <param name="array">
+        /// Numbers
+        /// </param>
+        /// <returns>
+        /// The greatest common divisor
+        /// </returns>
+        public static int FindGCD(out string time, params int[] array)
+        {
+            if (array.Length == 1)
+            {
+                throw new ArgumentException(nameof(array), "CGD counts only for 2 and more numbers");
+            }
+
+            Stopwatch stw = Stopwatch.StartNew();
+            int gcd = array[0];
+            for (int i = 1; i < array.Length; i++)
+            {
+                gcd = FindGCD(gcd, array[i]);
+            }
+
+            time = (stw.Elapsed).ToString();
+            stw.Stop();
+            return gcd;
+        }
+
+        /// <summary>
+        /// Finds GCD of 2 numbers <paramref name="a"/> and <paramref name="b"/> by Stein's algorithm 
+        /// </summary>
+        /// <param name="a">
+        /// The first number
+        /// </param>
+        /// <param name="b">
+        /// The second number
+        /// </param>
+        /// <returns>
+        /// The greatest common divisor of <paramref name="a"/> and <paramref name="b"/>
+        /// </returns>
+        public static int FindGCDBinary(int a, int b)
+        {
+            if (a == b)
+            {
+                return a;
+            }
+
+            if (a == 0)
+            {
+                return b;
+            }
+
+            if (b == 0)
+            {
+                return a;
+            }
+
+            a = Math.Abs(a);
+            b = Math.Abs(b);
+
+            if ((~a & 1) != 0)
+            {
+                if ((b & 1) != 0)
+                {
+                    return FindGCDBinary(a >> 1, b);
+                }
+                else
+                {
+                    return FindGCDBinary(a >> 1, b >> 1) << 1;
+                }
+            }
+
+            if ((~b & 1) != 0)
+            {
+                return FindGCDBinary(a, b >> 1);
+            }
+
+            if (a > b)
+            {
+                return FindGCDBinary((a - b) >> 1, b);
+            }
+
+            return FindGCDBinary((b - a) >> 1, a);
+        }
+
+        /// <summary>
+        /// Finds GCD of 3 and more numbers by Stein's algorithm
+        /// </summary>
+        /// <param name="array">
+        /// Numbers 
+        /// </param>
+        /// <returns>
+        /// GCD of 3 and more numbers
+        /// </returns>
+        public static int FindGCDBinary(params int[] array)
+        {
+            if (array.Length == 1)
+            {
+                throw new ArgumentException(nameof(array), "CGD counts only for 2 and more numbers");
+            }
+
+            int gcd = array[0];
+            for (int i = 1; i < array.Length; i++)
+            {
+                gcd = FindGCDBinary(gcd, array[i]);
+            }
+
+            return gcd;
+        }
+
+        /// <summary>
+        /// Finds GCD of 3 and more numbers by Stein's algorithm 
+        /// </summary>
+        /// <param name="time">
+        /// Out parameter, returns elapsed time for the algorithm 
+        /// </param>
+        /// <param name="array">
+        /// Numbers
+        /// </param>
+        /// <returns>
+        /// GCD of 3 and more numbers
+        /// </returns>
+        public static int FindGCDBinary(out string time, params int[] array)
+        {
+            if (array.Length == 1)
+            {
+                throw new ArgumentException(nameof(array), "CGD counts only for 2 and more numbers");
+            }
+
+            Stopwatch stw = Stopwatch.StartNew();
+            int gcd = array[0];
+            for (int i = 1; i < array.Length; i++)
+            {
+                gcd = FindGCDBinary(gcd, array[i]);
+            }
+
+            time = (stw.Elapsed).ToString();
+            stw.Stop();
+            return gcd;
+        }
+    }
+}
